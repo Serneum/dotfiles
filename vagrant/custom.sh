4cbc45e0e816312d5175e6985487207bbf56cc1d
Chris Rees <serneum@gmail.com>
2016-05-20 10:35:55 -0400

    Update custom.sh

--- vagrant/custom.sh
+++ vagrant/custom.sh
@@ -2,7 +2,7 @@ SETUP_FILE=/home/vagrant/setup.sh
 
 # custom.sh is run as the root user. Create a shell script that we can run as
 # the vagrant user
-cat <<EOF > setup.sh
+cat <<EOF > $SETUP_FILE
 # Clone settings repo
 git clone https://github.com/Serneum/settings.git
 
@@ -10,19 +10,26 @@ git clone https://github.com/Serneum/settings.git
 cd ~/settings/git
 sed -i.bak "s/USER_NAME/Chris Rees/g" .gitconfig
 sed -i.bak "s/USER_EMAIL/crees@covermymeds.com/g" .gitconfig
+
 mv .gitconfig ~/.gitconfig
 mv .gitignore ~/.gitignore
 
 # Copy over bash settings
 cd ../home
 mv .bashrc ~/.bashrc
-mv .bash ~/.bash
+if [ ! -d ~/.bash ]; then
+  mv .bash ~/
+fi
 
 # Set up Vim, Vundle, and install plugins from the .vimrc file
-mkdir ~/.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-mv .vimrc ~/.vimrc
-vim +PluginInstall +qall
+if [ ! -d ~/.vim ]; then
+  mkdir ~/.vim
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  mv .vimrc ~/.vimrc
+  
+  # Prevent the issue where Vim complains about input/output and causes the terminal to need to be closed
+  echo | echo | vim +PluginInstall +qall &>/dev/null
+fi
 
 # Remove the cloned repo
 cd ..
