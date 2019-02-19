83d7e1ceaa097bd3844a534e404454b0145dd4b5
Chris Rees <crees@covermymeds.com>
2019-02-19 14:11:35 -0500

    Switch to Antibody

--- zsh/antibody.sh
+++ zsh/antibody.sh
@@ -1,3 +1,7 @@
+source /Users/crees/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/autojump/autojump.plugin.zsh
+fpath+=( /Users/crees/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/autojump )
+source /Users/crees/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/vundle/vundle.plugin.zsh
+fpath+=( /Users/crees/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/vundle )
 source /Users/crees/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
 fpath+=( /Users/crees/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting )
 source /Users/crees/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
