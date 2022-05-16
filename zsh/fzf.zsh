cc15c20868b89980400173f37b06eb3eadbcc254
Chris Rees <serneum@gmail.com>
2022-05-16 16:10:39 -0400

    Fix fzf

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */Users/crees/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/Users/crees/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/Users/crees/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "/Users/crees/.fzf/shell/key-bindings.zsh"
