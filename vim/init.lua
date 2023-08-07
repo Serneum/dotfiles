b1e34d6ab4e11900c6e0b938b986051334686230
Chris Rees <serneum@gmail.com>
2023-08-07 17:29:10 -0400

    Fix indentation for init.lua

--- vim/init.lua
+++ vim/init.lua
@@ -1,6 +1,14 @@
 local opts = { noremap = true }
 vim.api.nvim_set_keymap('t', '<ESC>', '<C-\\><C-n>', opts)
 
+vim.opt.list = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.clipboard = unnamed
+
 local ensure_packer = function()
   local fn = vim.fn
   local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
@@ -18,47 +26,47 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   -- My plugins here
   use {
-  	'nvim-tree/nvim-tree.lua',
-  	requires = { 'nvim-tree/nvim-web-devicons' }
+    'nvim-tree/nvim-tree.lua',
+    requires = { 'nvim-tree/nvim-web-devicons' }
   }
   use {
-	'nvim-telescope/telescope.nvim',
-	branch = '0.1.x',
-	requires = { 'nvim-lua/plenary.nvim' }
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    requires = { 'nvim-lua/plenary.nvim' }
   }
   use {
-	'ibhagwan/fzf-lua',
-	requires = { "nvim-tree/nvim-web-devicons" }
+    'ibhagwan/fzf-lua',
+    requires = { "nvim-tree/nvim-web-devicons" }
   }
   use 'rcarriga/nvim-notify'
   use 'lewis6991/gitsigns.nvim'
   use {
-	'akinsho/toggleterm.nvim',
-	tag = '*',
-	config = function()
-  		require("toggleterm").setup()
-	end
+    'akinsho/toggleterm.nvim',
+    tag = '*',
+    config = function()
+      require("toggleterm").setup()
+    end
   }
   use {
-	"folke/which-key.nvim",
-  	config = function()
-    		vim.o.timeout = true
-    		vim.o.timeoutlen = 300
-    		require("which-key").setup {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-    		}
-  	end
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
   }
   use {
-	  'jdhao/whitespace.nvim',
-	  event = 'VimEnter'
+    'jdhao/whitespace.nvim',
+    event = 'VimEnter'
   }
   use {
-  	'glepnir/galaxyline.nvim',
-  	branch = 'main',
-  	requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    'glepnir/galaxyline.nvim',
+    branch = 'main',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
 
   -- Automatically set up your configuration after cloning packer.nvim
