07674e146936bedf99dc5b3683a09d2e1af02b55
Chris Rees <serneum@gmail.com>
2023-08-07 21:22:14 -0400

    Update Telescope with functionality to clear prompt

--- vim/plugin/telescope.lua
+++ vim/plugin/telescope.lua
@@ -1,6 +1,12 @@
+local actions = require("telescope.actions")
 require('telescope').setup {
   defaults = {
-    sorting_strategy = 'ascending'
+    sorting_strategy = 'ascending',
+    mappings = {
+      i = {
+        ["<C-u>"] = false
+      },
+    },
   }
 }
 
