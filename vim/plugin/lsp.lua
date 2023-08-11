2e984364f022d427cb83bfb69b687d97af5f3634
Chris Rees <serneum@gmail.com>
2023-08-11 11:12:37 -0400

    Tell Lua LSP that vim is a defined global

--- vim/plugin/lsp.lua
+++ vim/plugin/lsp.lua
@@ -6,6 +6,16 @@ require("mason-lspconfig").setup {
 require("mason-lspconfig").setup_handlers {
   function (server_name)
     require("lspconfig")[server_name].setup {}
+
+    require("lspconfig").lua_ls.setup {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim' }
+          }
+        }
+      }
+    }
   end
 }
 
