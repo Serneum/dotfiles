05c2c44d077890042128b3434dc7ba1ffd11a314
Chris Rees <serneum@gmail.com>
2023-08-11 11:00:29 -0400

    Add autocomplete functionality with LSP

--- vim/init.lua
+++ vim/init.lua
@@ -84,6 +84,10 @@ return require('packer').startup(function(use)
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
     'neovim/nvim-lspconfig',
+    'hrsh7th/nvim-cmp',         -- Autocompletion plugin
+    'hrsh7th/cmp-nvim-lsp',     -- LSP source for nvim-cmp
+    'saadparwaiz1/cmp_luasnip', -- Snippets source for nvim-cmp
+    'L3MON4D3/LuaSnip',         -- Snippets plugin
   }
 
   -- Git Blame
