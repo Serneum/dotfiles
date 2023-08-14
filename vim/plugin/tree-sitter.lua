5c1befb7659b16ebec0cb229561d0492c0937850
Chris Rees <serneum@gmail.com>
2023-08-14 09:46:56 -0400

    Add markdown_inline highlighting to tree-sitter

--- vim/plugin/tree-sitter.lua
+++ vim/plugin/tree-sitter.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-  ensure_installed = { "elixir", "javascript", "json", "lua", "ruby", "vim", "yaml" },
+  ensure_installed = { "dockerfile", "elixir", "javascript", "json", "lua", "markdown_inline", "ruby", "vim", "yaml" },
 
   highlight = {
     enable = true,
