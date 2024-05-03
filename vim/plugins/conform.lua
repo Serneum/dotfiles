e51af2036585de32defe013c4a06d76b7584ad35
Chris Rees <christopher.rees@eunasolutions.com>
2024-05-03 14:07:30 -0400

    Add Rubyfmt to Conform plugin

--- vim/plugins/conform.lua
+++ vim/plugins/conform.lua
@@ -1,6 +1,9 @@
 return {
 	"stevearc/conform.nvim",
 	opts = {
+		formatters_by_ft = {
+			ruby = { "rubocop", "rbfmt" },
+		},
 		formatters = {
 			shfmt = {
 				prepend_args = { "-i", "2", "-ci", "-bn" },
